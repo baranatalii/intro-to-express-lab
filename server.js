4305255b2f68b0cb9f65e55c656294a2c22526a2
Baranatalii <baranatalii@BARANs-MacBook-Air.local>
2026-01-06 16:05:48 -0500

    exercise 2

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ app.get("/greetings/:username", (req, res) => {
   res.send(`Hello there, ${req.params.username}!`);
 });
 
+app.get("/roll/:number", (req, res) => {
+  const max = req.params.number;
 
+  if (isNaN(max)) {
+    return res.send("You must specify a number.");
+  }
+
+  const rolledNumber = Math.floor(Math.random() * (parseInt(max) + 1));
+  res.send(`You rolled a ${rolledNumber}.`);
+});
 
 app.listen(3000, () => {
   console.log("Your server is running");
